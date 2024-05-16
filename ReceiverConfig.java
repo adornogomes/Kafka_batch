648e70bb9f2610951c96fb1016b8f1d122da7e7a
Daniel Adorno Gomes <32546263+adornogomes@users.noreply.github.com>
2024-05-16 23:12:46 +0100

    Create ReceiverConfig.java

---
+++ ReceiverConfig.java
@@ -0,0 +1,33 @@
+@EnableKafka
+@Configuration
+public class ReceiverConfig {
+  
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+  
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "batch");
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "4");
+        return props;
+    }
+  
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+  
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setBatchListener(true);
+        factory.setBatchErrorHandler(new BatchLoggingErrorHandler());
+        return factory;
+    }
+  
+}
